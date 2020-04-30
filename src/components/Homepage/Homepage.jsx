d303291f00bbc5ed0d34c8125fd958acac46637a
Brian <brian.lagaude@gmail.com>
2020-04-30 13:51:57 +0200

    refactor: end

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -11,6 +11,7 @@ class Homepage extends React.Component {
   render() {
     return (
       <div className="homepage__container">
+        <h1><strong className="titleRed">Webcam</strong><strong className="titleGrey">Express</strong></h1>
         <img className="homepage_logo" src="https://zupimages.net/up/20/18/clef.png" alt="Logo Webcam Express" />
         <p className="Explication">Bored to be alone at home ? You want to travel a bit ?</p>
         <p className="Explication">Join us in this epic adventure who will make you travel in the world with only one click !</p>
