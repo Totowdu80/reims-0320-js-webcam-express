52d423c8afb3867f151efde659f1d60959239184
Brian <brian.lagaude@gmail.com>
2020-04-30 02:14:56 +0200

    feat: add navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class NavBar extends React.Component{
+constructor(props){
+  super(props)
+  this.state={}
+}
+
+<section>
+  <nav>
+    <div></div>
+    <ul>
+      <li data-xcoord="0px" class="active">Home</li>
+      <li data-xcoord="160px">About</li>
+      <li data-xcoord="320px">Contact</li>
+      <li data-xcoord="480px">Store</li>
+    </ul>
+  </nav>
+</section>
+
+}
